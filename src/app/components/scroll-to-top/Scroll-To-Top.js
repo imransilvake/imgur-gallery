5d091445fe49edc67f49bba79d4e0a394b055615
Imran Khan <imransilvake@gmail.com>
2020-08-07 11:18:27 +0200

    [Refactor] scroll to top component

--- src/app/components/scroll-to-top/Scroll-To-Top.js
+++ src/app/components/scroll-to-top/Scroll-To-Top.js
@@ -2,8 +2,8 @@
 import React, { useEffect, useState } from 'react';
 
 // app
+import './Scroll-To-Top.scss';
 import ScrollTop from '../../../assets/svg/scroll-top.svg';
-import scrollToTop from './scrollToTop';
 
 /**
  * scroll to top
@@ -34,8 +34,24 @@ const ScrollTopTop = () => {
 		return () => window.removeEventListener('scroll', handleScroll);
 	}, [scrollView, setScrollView]);
 
+	/**
+	 * scroll to top of the page
+	 */
+	const scrollToTop = () => {
+		const scrollSpeed = 400;
+		const scrollDuration = scrollSpeed / 25;
+		const scrollStep = -window.scrollY / scrollDuration;
+		const scrollInterval = setInterval(() => {
+			if (window.scrollY !== 0) {
+				window.scrollBy(0, scrollStep);
+			} else {
+				clearInterval(scrollInterval);
+			}
+		}, 15);
+	};
+
 	return scrollView && (
-		<div className="ig-page-top">
+		<div className="ig-scroll-to-top">
 			<button type="button" onClick={scrollToTop}>
 				<img src={ScrollTop} alt="load more" />
 			</button>
